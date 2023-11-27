68bdd8b0a9b138b36cabf455d5eb44adb0ec39df
ali99944 <ali99944>
2023-11-27 18:23:31 +0200

    v1.8 update connection string

--- utils/mongoose_connection.js
+++ utils/mongoose_connection.js
@@ -4,7 +4,7 @@ import { mongodb_connection_string, mongodb_local_connection } from '../config.j
 
 export default new Promise(async (resolve, reject) => {
     try{
-        await connect(mongodb_local_connection).then(() => console.log('connected to mongodb'))
+        await connect(mongodb_connection_string).then(() => console.log('connected to mongodb'))
         return resolve(true) 
     }catch(e){
         return reject (new Error('Connection Error: ' + e.message))
